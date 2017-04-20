c7326a37dbcc78a334f5745e4769e2d941477171
Levi Starrett <levi@roxsoftware.com>
2017-04-20 15:22:03 -0400

    job #9455 starting move to jenkins

---
+++ install-build-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install -y git jenkins
+git clone https://github.com/leviathan747/buildmt.git .
+chown -R hudson .
+TMPFILE=`mktemp`
+sed 's@^JENKINS_HOME=.*$@JENKINS_HOME='$PWD'/buildmt/jenkins-home@g' /etc/default/jenkins > $TEMPFILE
+cp $TMPFILE /etc/default/jenkins
+/etc/init.d/jenkins start
