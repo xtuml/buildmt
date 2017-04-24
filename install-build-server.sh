7fd19895fd26328a8aa8e6b05c07f99c53f2e2ca
Levi Starrett <levi@roxsoftware.com>
2017-04-24 16:34:53 -0400

    job #9455 made change to allow cloning into non empty directory

--- install-build-server.sh
+++ install-build-server.sh
@@ -6,7 +6,10 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install -y git jenkins
-git clone https://github.com/leviathan747/buildmt.git --branch jenkins --depth 1 .
+mkdir temp-git
+git clone https://github.com/leviathan747/buildmt.git --branch jenkins --depth 1 temp-git
+mv temp-git/* .
+rm -rf temp-git
 TMPFILE=`mktemp`
 sed 's@^JENKINS_HOME=.*$@JENKINS_HOME='$PWD'/buildmt/jenkins-home@g' /etc/default/jenkins > $TMPFILE
 cp $TMPFILE /etc/default/jenkins
