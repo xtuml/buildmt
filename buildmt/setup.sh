7e0ad3f77371cfb69221de5c6fc618c6d1316ac2
Levi Starrett <levi@roxsoftware.com>
2017-04-24 15:18:39 -0400

    job #9455 update bridgepoint install

--- buildmt/setup.sh
+++ buildmt/setup.sh
@@ -16,6 +16,9 @@ su jenkins -c 'printf "newpass\nnewpass\n\n" | vncpasswd'
 cd $DIR
 wget https://s3.amazonaws.com/xtuml-releases/release-build/201703301540_v6.2.4/BridgePoint_v6.2.4-linux.gtk.x86_64.zip
 unzip BridgePoint_v6.2.4-linux.gtk.x86_64.zip
+TMPFILE=`mktemp`
+sed 's/WORKSPACE/WORKSPACE2/g' BridgePoint/tools/mc/bin/CLI.sh > $TMPFILE
+mv $TMPFILE BridgePoint/tools/mc/bin/CLI.sh
 
 # install plugins
 cd $DIR/jenkins-home
