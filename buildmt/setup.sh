44079d9e53e9f0f791dba74c012a4b5ee088ddff
Levi Starrett <levi@roxsoftware.com>
2017-04-25 12:08:48 -0400

    job #9455 BP install config update

--- buildmt/setup.sh
+++ buildmt/setup.sh
@@ -17,6 +17,7 @@ su jenkins -c 'printf "newpass\nnewpass\n\n" | vncpasswd'
 cd $DIR
 wget http://s3.amazonaws.com/xtuml-releases/nightly-build/org.xtuml.bp.product-linux.gtk.x86_64.zip
 unzip org.xtuml.bp.product-linux.gtk.x86_64.zip
+mv org.xtuml.bp.product-linux.gtk.x86_64.zip BridgePoint
 TMPFILE=`mktemp`
 sed 's/WORKSPACE/WORKSPACE2/g' BridgePoint/tools/mc/bin/CLI.sh > $TMPFILE
 mv $TMPFILE BridgePoint/tools/mc/bin/CLI.sh
