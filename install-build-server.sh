42e19fd1e2dabbdb9fdf6ba5eaaf13f20a77d731
Levi Starrett <levi@roxsoftware.com>
2017-04-25 14:13:55 -0400

    job #9455 refining install script

--- install-build-server.sh
+++ install-build-server.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-useradd -p `perl -e 'print crypt("password", "salt"),"\n"'` jenkins
+
+# add build group and jenkins user
 groupadd build
-usermod -G build -a jenkins
+useradd -p `perl -e 'print crypt("password", "salt"),"\n"'` -g build jenkins
+
+# install jenkins and git
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install -y git jenkins
+
+# clone buildmt repo
 mkdir temp-git
 git clone https://github.com/leviathan747/buildmt.git --branch jenkins --depth 1 temp-git
 mv temp-git/* .
 mv temp-git/.[!.]* .
 rm -rf temp-git
+
+# modify jenkins home and umask of jenkins process
 TMPFILE=`mktemp`
 sed 's@^JENKINS_HOME=.*$@JENKINS_HOME='$PWD'/buildmt/jenkins-home@g' /etc/default/jenkins > $TMPFILE
 cp $TMPFILE /etc/default/jenkins
+sed -r 's@^DAEMON_ARGS="(.*)"@DAEMON_ARGS="\1 --umask=002"@g' /etc/init.d/jenkins > $TMPFILE
+cp $TMPFILE /etc/init.d/jenkins
+
+# run setup
 bash buildmt/setup.sh
+
+# fixup permissions
 chown -R jenkins:build .
+
+# restart jenkins
 /etc/init.d/jenkins restart
