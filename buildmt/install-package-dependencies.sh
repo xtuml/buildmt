493723676799ef9d875aafdaf8ec66408b3b13da
Levi Starrett <levi@roxsoftware.com>
2019-02-11 10:20:20 -0500

    job #11496 updates for xfce setup

--- buildmt/install-package-dependencies.sh
+++ buildmt/install-package-dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-TMPFILE="/tmp/pkg_dep_install.log"
-echo "Installing dependencies. Install log at $TMPFILE."
-apt install -y gtk2.0 vim vnc4server maven unzip pypy awscli zip flex bison clang gcc-mingw-w64-x86-64 xsltproc xrdp xfce4 > $TMPFILE
+echo "Installing dependencies."
+apt install -y gtk2.0 vim vnc4server maven unzip pypy awscli zip flex bison clang gcc-mingw-w64-x86-64 xsltproc xrdp xfce4 xvfb
