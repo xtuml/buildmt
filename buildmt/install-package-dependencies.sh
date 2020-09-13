1052531168710fc75c77c58acb83f409657b7ddc
tlondon-techmod <travis.london@gmail.com>
2020-09-13 12:40:38 -0500

    visualvm not visual-vm

--- buildmt/install-package-dependencies.sh
+++ buildmt/install-package-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 echo "Installing dependencies."
-apt install -y gtk2.0 vim vnc4server maven unzip pypy awscli zip flex bison clang gcc-mingw-w64-x86-64 xsltproc xrdp xfce4 xvfb visual-vm openjdk-11-jdk
+apt install -y gtk2.0 vim vnc4server maven unzip pypy awscli zip flex bison clang gcc-mingw-w64-x86-64 xsltproc xrdp xfce4 xvfb visualvm openjdk-11-jdk
