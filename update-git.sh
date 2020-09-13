ddb9d4e4b736ca22f048b4843cb1b5e25a8340f4
tlondon-techmod <travis.london@gmail.com>
2020-09-13 07:05:56 -0500

    Only reset and update buildmt if not dealing with fork/branch configuration

--- update-git.sh
+++ update-git.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
-git reset --hard
-git clean -df .
-git pull -f
+# only update if the branch is master, otherwise a specific
+# fork/branch is being used and is up to date
+BRANCH=`git symbolic-ref --short -q HEAD`
+if [[ "$BRANCH" == "master" ]]; then 
+  git reset --hard
+  git clean -df .
+  git pull -f
+fi
 bash buildmt/setup.sh >> /var/log/jenkins/build_server_setup.log 2>&1
